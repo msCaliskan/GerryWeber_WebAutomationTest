4a24b325e6baa0a68ea03c7f9eb79b32bdd08769
musa.caliskan <musa.caliskan@inveon.com>
2022-07-05 11:36:50 +0300

    HomePage class updated

--- src/test/java/com/gerryweber/pages/HomePage.java
+++ src/test/java/com/gerryweber/pages/HomePage.java
@@ -8,58 +8,63 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
-    @FindBy(xpath = "(//*[contains(@class,'MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium')])[2]") public WebElement close_Loc;
+    @FindBy(xpath = "(//*[contains(@class,'MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium')])[2]")
+    public WebElement close_Loc;
 
-    @FindBy(css = "#email") public WebElement email_Loc;
+    @FindBy(css = "#email")
+    public WebElement email_Loc;
 
-    @FindBy(css = "#password") public WebElement password_Loc;
+    @FindBy(css = "#password")
+    public WebElement password_Loc;
 
-    @FindBy(css = "#mui-1") public WebElement searchBox_Loc;
+    @FindBy(css = "#mui-1")
+    public WebElement searchBox_Loc;
 
 
-    public void gotoHomePage(){
+    public void gotoHomePage() {
         Driver.get().get(ConfigurationReader.get("url"));
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(5);
 
         JavascriptExecutor jse = (JavascriptExecutor) Driver.get();
         WebElement accept = (WebElement) jse.executeScript("return document.querySelector('#usercentrics-root').shadowRoot.querySelector('#focus-lock-id > div.sc-furwcr.kbclTA > div > div.sc-bYoBSM.egarKh > div > div > div.sc-dlVxhl.bEDIID > div > button:nth-child(3)')");
-       BrowserUtils.clickWithJS(accept);
-       BrowserUtils.waitFor(1);
-       close_Loc.click();
+        BrowserUtils.clickWithJS(accept);
+        BrowserUtils.waitFor(1);
+        close_Loc.click();
 
     }
 
-    public void login(){
-    String email = ConfigurationReader.get("user_email");
-    String password = ConfigurationReader.get("user_password");
+    public void login() {
+        String email = ConfigurationReader.get("user_email");
+        String password = ConfigurationReader.get("user_password");
 
-    email_Loc.sendKeys(email);
-    BrowserUtils.waitFor(1);
-    password_Loc.sendKeys(password);
+        email_Loc.sendKeys(email);
+        BrowserUtils.waitFor(1);
+        password_Loc.sendKeys(password);
     }
 
-    public static void checkHomePage(){
-        String expectedUrl ="https://www.gerryweber.com.tr/tr-TR";
+    public static void checkHomePage() {
+        String expectedUrl = "https://www.gerryweber.com.tr/tr-TR";
         String actualUrl = Driver.get().getCurrentUrl();
-        Assert.assertEquals(expectedUrl,actualUrl);
+        Assert.assertEquals(expectedUrl, actualUrl);
     }
 
-    public void searchBox(String string){
-        searchBox_Loc.sendKeys(string+ Keys.ENTER);
+    public void searchBox(String string) {
+        searchBox_Loc.sendKeys(string + Keys.ENTER);
 
         BrowserUtils.waitFor(1);
 
     }
 
-    public void invalidEmailCheck(){
+    public void invalidEmailCheck() {
         email_Loc.sendKeys(ConfigurationReader.get("guest_email"));
         password_Loc.sendKeys(ConfigurationReader.get("user_password"));
     }
-    public void invalidPasswordCheck(){
-       email_Loc.sendKeys(ConfigurationReader.get("user_email"));
-       password_Loc.sendKeys("Inveon34...");
+
+    public void invalidPasswordCheck() {
+        email_Loc.sendKeys(ConfigurationReader.get("user_email"));
+        password_Loc.sendKeys("Inveon34...");
     }
 
 }
